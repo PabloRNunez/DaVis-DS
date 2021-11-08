e915a9eb92c0f65be70828c0a19ef9c1474819b6
PabloRNunez <pablorduch1995@gmail.com>
2021-11-08 19:36:21 +0000

    Merged DS1 dictionaries

--- variables.py
+++ variables.py
@@ -8,7 +8,7 @@ ds_a_min = pd.read_excel('excels/ds_shields_min.xlsx')
 ds_a_max = pd.read_excel('excels/ds_shields_max.xlsx')
 
 
-variables = [
+gen_vars = [
     'set', 'name', 'level', 'position', 'poise', 'weight', 
     'phy_reduction', 'mag_reduction', 'fir_reduction', 'lig_reduction',
     'ble_resist', 'poi_resist', 'cur_resist', 
@@ -19,7 +19,7 @@ variables = [
     'str_req', 'dex_req', 'int_req', 'fai_req',
     'class', 'upgrade_path', 'status_effect']
 
-ft_variables = [
+ft_vars = [
     'poise', 'weight', 
     'phy_reduction', 'mag_reduction', 'fir_reduction', 'lig_reduction',
     'ble_resist', 'poi_resist', 'cur_resist','bleed','poison','frost',
@@ -27,17 +27,17 @@ ft_variables = [
     'phy_damage', 'mag_damage', 'fir_damage', 'lig_damage','dar_damage', 'tot_damage',
     'str_bonus', 'dex_bonus', 'int_bonus', 'fai_bonus',
     'str_req', 'dex_req', 'int_req', 'fai_req']
-fl_variables = [
+fl_vars = [
     'set', 'name', 'position',
     'class', 'upgrade_path', 'status_effect',
     'buffable','skill','damage_type']
 
-cod_variables = [
+cod_vars = [
     'str_bonus', 'dex_bonus', 'int_bonus', 'fai_bonus', 'level']
-cn_variables = [
+cn_vars = [
     'set', 'name', 'position',
     'class', 'upgrade_path', 'status_effect','buffable','damage_type','skill']
-cc_variables = [
+cc_vars = [
     'poise', 'weight', 
     'phy_reduction', 'mag_reduction', 'fir_reduction', 'lig_reduction',
     'ble_resist', 'poi_resist', 'cur_resist','bleed','poison','frost',
@@ -64,24 +64,14 @@ ds1_se_d = {
     'Poison':'green', 
     'Toxic':'darkgreen'}
 
-ds1_a_d = {
+ds1_d = {
     'set':'Set', 
-    'name':'Name', 
-    'position':'Position', 
-    'poise':'Poise', 
-    'weight':'Weight',
-    'phy_reduction':'Physical Reduction', 
-    'mag_reduction':'Magic Reduction',
-    'fir_reduction':'Fire Reduction', 
-    'lig_reduction':'Lightning Reduction',
-    'ble_resist':'Bleed Resistance', 
-    'poi_resist':'Poison Resistance', 
-    'cur_resist':'Curse Resistance'}
-ds1_w_d = {
     'name':'Name', 
     'level':'Level', 
     'class':'Class', 
+    'position':'Position', 
     'stability':'Stability', 
+    'poise':'Poise', 
     'weight':'Weight',
     'phy_damage':'Physical Damage', 
     'mag_damage':'Magic Damage', 
@@ -101,34 +91,10 @@ ds1_w_d = {
     'str_req':'Strength Requirement', 
     'dex_req':'Dexterity Requirement',
     'int_req':'Intelligence Requirement', 
-    'fai_req':'Faith Requirement'}
-ds1_s_d = {
-    'name':'Name', 
-    'level':'Level', 
-    'class':'Class', 
-    'stability':'Stability', 
-    'weight':'Weight',
-    'phy_damage':'Physical Damage', 
-    'mag_damage':'Magic Damage', 
-    'fir_damage':'Fire Damage', 
-    'lig_damage':'Lightning Damage', 
-    'tot_damage':'Total Damage',
-    'upgrade_path':'Upgrade Path',
-    'phy_reduction':'Physical Reduction', 
-    'mag_reduction':'Magic Reduction',
-    'fir_reduction':'Fire Reduction', 
-    'lig_reduction':'Lightning Reduction',
+    'fai_req':'Faith Requirement',
     'ble_resist':'Bleed Resistance', 
     'poi_resist':'Poison Resistance', 
-    'cur_resist':'Curse Resistance',
-    'str_bonus':'Strength Bonus', 
-    'dex_bonus':'Dexterity Bonus',
-    'int_bonus':'Intelligence Bonus', 
-    'fai_bonus':'Faith Bonus',
-    'str_req':'Strength Requirement', 
-    'dex_req':'Dexterity Requirement',
-    'int_req':'Intelligence Requirement', 
-    'fai_req':'Faith Requirement'}
+    'cur_resist':'Curse Resistance'}
 
 ds3_w_d = {
     'name':'Name', 
@@ -162,3 +128,17 @@ ds3_w_d = {
     'dex_req':'Dexterity Requirement',
     'int_req':'Intelligence Requirement', 
     'fai_req':'Faith Requirement'}
+
+command_list = ["data:", "filter:", "plot:", "color:", "label:"]
+
+filter_list = ["since", "until", "only"]
+filter_combine = ["and", "or"]
+
+success_response = "\n [+] Your plot was successfully completed."
+err_response = "\n [-] ERROR: "
+err_response_data = "dataset was not defined correclty."
+err_response_filter = "filters were not defined correclty."
+err_response_vars = "plot variables were not defined correclty."
+err_response_color = "color variable was not defined correclty."
+err_response_label = "label was not defined correclty."
+err_response_command = "a basic command is missing."
